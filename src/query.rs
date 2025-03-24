e2903125f11923cf89f95e1bc68cb88a22127f8e
KviringAleksei <alex@kviring.com>
2025-03-24 11:15:28 +0500

    variable support in config

--- src/query.rs
+++ src/query.rs
@@ -1,10 +1,29 @@
 pub fn get_queries(config_content: &str) -> Vec<String> {
-    config_content
+    let lines: Vec<String> = config_content
         .lines()
         .map(|line| line.trim().to_string())
         .filter(|line| !line.is_empty())
         .filter(|line| !line.starts_with("#"))
-        .collect()
+        .collect();
+
+    let variables: Vec<Vec<&str>> = lines
+        .iter()
+        .filter(|line| line.starts_with("$"))
+        .map(|line| line[1..].split("=").clone().collect::<Vec<&str>>())
+        .collect();
+    let mut query = vec![];
+    lines
+        .iter()
+        .filter(|line| !line.starts_with("$"))
+        .for_each(|line| {
+            let mut line = line.to_string();
+            variables.iter().for_each(|var| {
+                let name = format!("${}$", var[0]);
+                line = line.replace(name.as_str(), &var[1]);
+            });
+            query.push(line);
+        });
+    query
 }
 
 #[cfg(test)]
@@ -18,7 +37,8 @@ mod test {
         let mut file = NamedTempFile::new().unwrap();
         file.write(
             r#"
-            {job="my-job"}
+            $value=super-job
+            {job="$value$"}
             {job="another-job"}
             # комментарий
             {app="my-app"} | logfmt
@@ -28,8 +48,9 @@ mod test {
         .unwrap();
         let config = std::fs::read_to_string(file.path()).unwrap();
         let queries = get_queries(config.as_str());
+        println!("queries: {:?}", queries);
         assert_eq!(queries.len(), 3);
-        assert_eq!(queries[0], "{job=\"my-job\"}");
+        assert_eq!(queries[0], "{job=\"super-job\"}");
         assert_eq!(queries[1], "{job=\"another-job\"}");
         assert_eq!(queries[2], "{app=\"my-app\"} | logfmt");
     }
